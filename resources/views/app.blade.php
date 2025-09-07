f918ee79d908e29204d80481104cda2debfb2cdd
Dikhsantibong <tibongdikhsan@gmail.com>
2025-09-07 23:56:45 +0500

    perbaikan

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,8 @@
     <title>Desa Rakadua</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.3/dist/chart.umd.min.js"></script>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
     <div id="app"></div>
